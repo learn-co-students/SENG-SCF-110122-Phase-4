87a35074a0f3051d4d1f6945c6d7de6d3adc4e21
Shelby Talbert <shelby.talbert@flatironschool.com>
2023-03-07 12:47:12 -0500

    ready for lecture

---
+++ 02-client-server-communications/flatiron-theatre/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :productions
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # Custom route: get "/endpoint", to: "controller#action"
+end
