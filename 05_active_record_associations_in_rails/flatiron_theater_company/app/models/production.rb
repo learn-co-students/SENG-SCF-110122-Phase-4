3cc2df795c4d8f760f971ca5a70891d5b2bbf295
Shelby Talbert <shelby.talbert@flatironschool.com>
2023-03-15 12:54:46 -0400

    code ready for lecture

---
+++ 05_active_record_associations_in_rails/flatiron_theater_company/app/models/production.rb
@@ -0,0 +1,16 @@
+class Production < ApplicationRecord
+    validates :title, :genre, :budget, :director, presence: true
+    validates :title, uniqueness: true
+    validates :budget, numericality: { greater_than: 0 }
+    validates :description, length: { in: 5..250, message: "Please submit a description between 5 and 250 characters" }
+    validates :image, allow_blank: true, format: { with: /\.(png|jpg|jpeg)\z/, message: "Please submit a jpeg/jpg or png file." }
+
+    validate :title, :no_les_miz
+
+    def no_les_miz
+        if self.genre == "Musical" && self.title.include?("Miserables")
+            #adding to the errors hash
+            errors.add(:title, "Sorry we don't do Les Miz here.")
+        end
+    end
+end
