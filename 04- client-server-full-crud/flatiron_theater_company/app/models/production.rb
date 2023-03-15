644d5586e8187cfd3e405df23d33670a57fd0858
Shelby Talbert <shelby.talbert@flatironschool.com>
2023-03-15 12:28:05 -0400

    updated directory name

--- 04- client-server-full-crud/flatiron_theater_company/app/models/production.rb
+++
@@ -1,9 +0,0 @@
-class Production < ApplicationRecord
-    validates :title, :genre, :budget, :director, presence: true
-    validates :title, uniqueness: true
-    validates :budget, numericality: { greater_than: 0 }
-    validates :description, length: { in: 5..250, message: "Please submit a description between 5 and 250 characters" }
-    # new validation using regex
-    validates :image, allow_blank: true, format: { with: /\.(png|jpg|jpeg)\z/, message: "Please submit a jpeg/jpg or png file." }
-  
-end
