4e7c55549757f17cd72a8b8c23c9dd6df8270e64
Shelby Talbert <shelby.talbert@flatironschool.com>
2023-03-16 15:15:47 -0400

    Update seeds.rb

--- 05_active_record_associations_in_rails/flatiron_theater_company/db/seeds.rb
+++ 05_active_record_associations_in_rails/flatiron_theater_company/db/seeds.rb
@@ -5,27 +5,27 @@ p2 = Production.create(title: 'Cats', genre: 'Musical', director:'Andrew Lloyd W
 
 p3 = Production.create(title: 'Carmen', genre: 'Opera', director:'Georges Bizet', description:'Set in southern Spain this is the story of the downfall of Don José, a naïve soldier who is seduced by the wiles of the fiery and beautiful Carmen.', budget: 200000.00, image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Prudent-Louis_Leray_-_Poster_for_the_premi%C3%A8re_of_Georges_Bizet%27s_Carmen.jpg/300px-Prudent-Louis_Leray_-_Poster_for_the_premi%C3%A8re_of_Georges_Bizet%27s_Carmen.jpg', ongoing:false)
 
-# p3 = Production.create(title: 'Hamilton', genre: 'Musical', director:'Lin-Manuel Miranda', description:'An American Musical is a sung-and-rapped-through musical by Lin-Manuel Miranda. It tells the story of American Founding Father Alexander Hamilton.', budget: 400000.00, image:'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Hamilton-poster.jpg/220px-Hamilton-poster.jpg', ongoing:false)
+p3 = Production.create(title: 'Hamilton', genre: 'Musical', director:'Lin-Manuel Miranda', description:'An American Musical is a sung-and-rapped-through musical by Lin-Manuel Miranda. It tells the story of American Founding Father Alexander Hamilton.', budget: 400000.00, image:'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Hamilton-poster.jpg/220px-Hamilton-poster.jpg', ongoing:false)
 
 
-# u1 = User.create(name:'lin', email:'lin@gmail.com', password:'1234')
-# u2 = User.create(name:'judy', email:'judy@gmail.com', password:'1234')
-# 20.times{User.create(name:Faker::Name.name, email:Faker::Internet.free_email, password:'1234' )}
+u1 = User.create(name:'lin', email:'lin@gmail.com', password:'1234')
+u2 = User.create(name:'judy', email:'judy@gmail.com', password:'1234')
+20.times{User.create(name:Faker::Name.name, email:Faker::Internet.free_email, password:'1234' )}
 
-# hamlet_roles = ['Hamlet', 'Ophelia', 'Polonius', 'Laertes', 'Horatio', 'Gertrude', 'Ghost' ]
-# hamlet_roles.each{|hr| CastMember.create(name:Faker::Name.name, role:hr, production:p1)}
+hamlet_roles = ['Hamlet', 'Ophelia', 'Polonius', 'Laertes', 'Horatio', 'Gertrude', 'Ghost' ]
+hamlet_roles.each{|hr| CastMember.create(name:Faker::Name.name, role:hr, production:p1)}
 
-# cats_roles = ['Mr. Mistoffelees', 'Bombalurina', 'Rumpletezer', 'Grizabella']
-# cats_roles.each{|cr| CastMember.create(name:Faker::Name.name, role:cr, production:p2)}
+cats_roles = ['Mr. Mistoffelees', 'Bombalurina', 'Rumpletezer', 'Grizabella']
+cats_roles.each{|cr| CastMember.create(name:Faker::Name.name, role:cr, production:p2)}
 
-# carmen_roles = ['Carmen', 'Escamillo', 'Jose', 'Mercedes', 'Dancaire']
-# carmen_roles.each{|cr|CastMember.create(name:Faker::Name.name, role:cr, production:p3)}
+carmen_roles = ['Carmen', 'Escamillo', 'Jose', 'Mercedes', 'Dancaire']
+carmen_roles.each{|cr|CastMember.create(name:Faker::Name.name, role:cr, production:p3)}
 
-# hamilton_roles = ['Alexander Hamilton', 'King George III', 'Marquis de Lafayett', 'Angelica Schuyler Church', 'Peggy Schuyler', 'Thomas Jefferson']
-# hamilton_roles.each{|hr|CastMember.create(name:Faker::Name.name, role:hr, production:p4)}
+hamilton_roles = ['Alexander Hamilton', 'King George III', 'Marquis de Lafayett', 'Angelica Schuyler Church', 'Peggy Schuyler', 'Thomas Jefferson']
+hamilton_roles.each{|hr|CastMember.create(name:Faker::Name.name, role:hr, production:p4)}
 
-# 60.times{Ticket.create(user: User.all.sample, production: Production.all.sample, price:65.00)}
+60.times{Ticket.create(user: User.all.sample, production: Production.all.sample, price:65.00)}
 
-# Ticket.create(user:u1, production:p2, price:65.00)
-# Ticket.create(user:u2, production:p2, price:65.00)
-# Ticket.create(user:u2, production:p1, price:65.00)
+Ticket.create(user:u1, production:p2, price:65.00)
+Ticket.create(user:u2, production:p2, price:65.00)
+Ticket.create(user:u2, production:p1, price:65.00)
