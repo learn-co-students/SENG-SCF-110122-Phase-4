61cbb2ad4b34d9ad929690b4dcc06d304065e797
Shelby Talbert <shelby.talbert@flatironschool.com>
2023-03-16 17:10:49 -0400

    Update productions_controller.rb

--- 05_active_record_associations_in_rails/flatiron_theater_company/app/controllers/productions_controller.rb
+++ 05_active_record_associations_in_rails/flatiron_theater_company/app/controllers/productions_controller.rb
@@ -18,7 +18,7 @@ class ProductionsController < ApplicationController
 
     def update 
         production = Production.find(params[:id])
-        production.update!(production_params)
+        production.update(production_params)
         render json: production, status: :accepted
     end 
 
